9dc97e1919cf8ea50b1e4854171b201f217de61e
Zachary Shaw <zachthm@gmail.com>
2025-11-15 12:00:29 -0700

    add basic grid logic, plant placing with left mouse

--- Scripts/bomber-bug.gd
+++ Scripts/bomber-bug.gd
@@ -5,18 +5,18 @@ class_name BomberBug
 const bomber_bug_projectile_scene: PackedScene = preload("res://Scenes/bomber-projectile.tscn")
 @export var range: float = 200
 
+var projectile_node: Node2D
+
 func attack():
 	super.attack()
-	
-	print("i'd be attacking now!")
 	var inst = bomber_bug_projectile_scene.instantiate()
 	add_child(inst)
 	
 	var direction = (self.reached_target.position - self.position).normalized()
-	var inst_node: Node2D = inst
-	inst_node.look_at(direction)
-	inst_node.translate(direction * range/2)
-	sprite = inst_node.get_node("Sprite")
+	projectile_node = inst
+	projectile_node.look_at(direction)
+	projectile_node.translate(direction * range/2)
+	sprite = projectile_node.get_node("Sprite")
 	sprite.set_instance_shader_parameter("attack_timer", self.attack_timer)
 	
 	var plant_script: Plant = self.reached_target
@@ -31,6 +31,9 @@ func initialize(id: String) -> void:
 func move_to_target():
 	super.move_to_target()
 	if (self.reached_target == null):
+		if (self.projectile_node != null):
+			self.projectile_node.queue_free()
+			self.projectile_node = null
 		var direction = (self.target.position - self.position).normalized()
 		self.position += direction * self.move_speed
 		
--- Scripts/plant.gd
+++ Scripts/plant.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 class_name Plant
 
+@export var grid_width = 1
+@export var grid_height = 1
+
 var total_hp: int = 100
 var range: float = 250.0
 var current_target: Node2D = null
@@ -17,11 +20,14 @@ func attack():
 func take_damage(damage: float):
 	if (self.total_hp > 0):
 		self.total_hp -= damage
-	print("I have %d hp left" % self.total_hp)
+	#print("I have %d hp left" % self.total_hp)
 	
 func set_target(target: Node2D):
 	if (target != null):
 		self.current_target = target
+		
+func get_dimensions() -> Vector2:
+	return Vector2(self.grid_width, self.grid_height)
 
 func die():
 	queue_free()
